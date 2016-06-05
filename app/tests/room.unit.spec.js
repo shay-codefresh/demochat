5187b9affacad0f5656d8e7697e3b11ec215fba2
Itai Gendler <itai@codefresh.io>
2016-06-05 16:38:19 +0300

    change

--- app/tests/room.unit.spec.js
+++ app/tests/room.unit.spec.js
@@ -9,13 +9,16 @@ describe('CfHpeMocha', function () {
     it('Should report test result passed #3', function () {
     });
 
-    it('Should report test result passed #4', function () {
+    it('Should report test result failed #4', function () {
+        throw new Error("test has failed");
     });
 
-    it('Should report test result passed #5', function () {
+    it('Should report test result failed #5', function () {
+        throw new Error("test has failed");
     });
 
-    it('Should report test result passed #6', function () {
+    it('Should report test result failed #6', function () {
+        throw new Error("test has failed");
     });
 
 });
