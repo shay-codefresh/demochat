f61121db3d34d0f1c5524b864233b2b341593777
Itai Gendler <itai@codefresh.io>
2016-06-05 16:36:27 +0300

    all tests will fail

--- app/tests/room.unit.spec.js
+++ app/tests/room.unit.spec.js
@@ -1,21 +1,27 @@
 describe('CfHpeMocha', function () {
 
     it('Should report test result passed #1', function () {
+        throw new Error("failed");
     });
 
     it('Should report test result passed #2', function () {
+        throw new Error("failed");
     });
 
     it('Should report test result failed #3', function () {
+        throw new Error("failed");
     });
 
     it('Should report test result failed #4', function () {
+        throw new Error("failed");
     });
 
     it('Should report test result passed #5', function () {
+        throw new Error("failed");
     });
 
     it('Should report test result passed #6', function () {
+        throw new Error("failed");
     });
 
 });
