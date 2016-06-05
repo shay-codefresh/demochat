a3abf6a11f66d89eac228caf6dacfc2b21c0d083
Itai Gendler <itai@codefresh.io>
2016-06-05 16:35:41 +0300

    all tests will pass

--- app/tests/room.unit.spec.js
+++ app/tests/room.unit.spec.js
@@ -1,5 +1,21 @@
-describe('entity tests', function(){
-   it('create user', function(done){
-      done();
-   })
-})
+describe('CfHpeMocha', function () {
+
+    it('Should report test result passed #1', function () {
+    });
+
+    it('Should report test result passed #2', function () {
+    });
+
+    it('Should report test result failed #3', function () {
+    });
+
+    it('Should report test result failed #4', function () {
+    });
+
+    it('Should report test result passed #5', function () {
+    });
+
+    it('Should report test result passed #6', function () {
+    });
+
+});
